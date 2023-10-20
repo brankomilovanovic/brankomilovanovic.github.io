5ade7d36ec84adbea5473a6a810a19a47dcdda4a
Branko Milovanovic <brankomilovanovic2000@gmail.com>
2023-10-21 01:44:17 +0200

    upload changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
