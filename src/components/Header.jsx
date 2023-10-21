414d6386f5ed78851b47ce86ff0a37379937bdfa
Branko Milovanovic <brankomilovanovic2000@gmail.com>
2023-10-22 01:49:14 +0200

    update base components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,87 @@
-import React, { useState } from "react";
+import { IconButton } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const navbarRef = useRef(null);
   const [toggleMobileMenu, setToggleMobileMenu] = useState(false);
+
+  useEffect(() => {
+    if (toggleMobileMenu) {
+      document.addEventListener("click", handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [toggleMobileMenu]);
+
+  const handleOnClickLink = () => {
+    setToggleMobileMenu(false);
+  };
+
+  const handleOutsideClick = (e) => {
+    if (
+      navbarRef?.current &&
+      !navbarRef?.current?.contains(e.target) &&
+      e?.target?.nodeName?.toLowerCase() !== "img" &&
+      toggleMobileMenu
+    ) {
+      setToggleMobileMenu(false);
+    }
+  };
 
   return (
     <div className="header">
       <div className="navbar-container">
-        <div>
-          <NavLink to={"/"}>Branko Milovanovic | Portfolio</NavLink>
+        <NavLink to={"/"} className="navbar-name">
+          Branko Milovanovic
+        </NavLink>
+        <div
+          ref={navbarRef}
+          className={`navbar-links-container ${toggleMobileMenu && "active"}`}
+        >
+          <NavLink to={"/"} onClick={handleOnClickLink} className="navbar-link">
+            Home
+          </NavLink>
+          <NavLink
+            to={"/about-me"}
+            onClick={handleOnClickLink}
+            className="navbar-link"
+          >
+            About Me
+          </NavLink>
+          <NavLink
+            to={"/projects"}
+            onClick={handleOnClickLink}
+            className="navbar-link"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to={"/contact"}
+            onClick={handleOnClickLink}
+            className="navbar-link"
+          >
+            Contact
+          </NavLink>
+          <NavLink
+            to={"/download-resume"}
+            onClick={handleOnClickLink}
+            className="navbar-link"
+          >
+            Download Resume
+          </NavLink>
         </div>
-        <div className="navbar-links-container">
-          <NavLink to={"/about-me"}>About me</NavLink>
-          <NavLink to={"/contact-me"}>Contact me</NavLink>
+        <div className="hamburger">
+          <IconButton
+            onClick={() => setToggleMobileMenu((prevState) => !prevState)}
+          >
+            {!toggleMobileMenu ? (
+              <img src="/assets/images/hamburger-menu.svg" />
+            ) : (
+              <img src="/assets/images/close.svg" />
+            )}
+          </IconButton>
         </div>
       </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,55 @@
-import React from "react";
+import { LinearProgress } from "@mui/material";
+import React, { useEffect, useRef } from "react";
 
 const Home = () => {
+  const progressRef = useRef(null);
+
+  const [progress, setProgress] = React.useState(30);
+  const [buffer, setBuffer] = React.useState(10);
+
+  useEffect(() => {
+    progressRef.current = () => {
+      const diff = Math.random() * 10;
+      const diff2 = Math.random() * 10;
+      setProgress(progress + diff);
+      setBuffer(progress + diff + diff2);
+    };
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      progressRef.current();
+    }, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className="home">
-      <p>Hi, I'm Branko Milovanovic</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          paddingTop: "50px",
+        }}
+      >
+        <span style={{ fontSize: "24px" }}>Hi, I'm Branko Milovanović</span>
+        <span style={{ fontSize: "24px" }}>
+          Welcome to my portfolio website
+        </span>
+        <div style={{ width: "80%", marginTop: "50px" }}>
+          <span style={{ fontSize: "24px" }}>Working in progress...</span>
+
+          <LinearProgress
+            variant="buffer"
+            value={progress}
+            valueBuffer={buffer}
+          />
+        </div>
+      </div>
     </div>
   );
 };
